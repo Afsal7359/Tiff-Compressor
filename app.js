63a28ceb402549795ca9fc56f77e18025235fd75
Afsal7359 <afsal4771@gmail.com>
2024-07-23 19:41:42 +0530

    fix first issue

--- app.js
+++ app.js
@@ -22,12 +22,10 @@ app.use(cors());
 app.use(morgan('dev'))
 
 
+app.use(express.json({ limit: '200mb' })); // Increase limit as needed
+app.use(express.urlencoded({ extended: true, limit: '200mb' })); 
 
 
-app.use(express.json({
-  limit: '50mb'
-}));
-app.use(express.urlencoded({ extended: true }))
 
 
 //root
